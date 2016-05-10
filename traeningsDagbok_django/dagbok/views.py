5a572289f6216780c78d3332ec90e01d27921596
Emma Andersson <emmaandersson@Emmas-MacBook-Pro.local>
2016-05-10 10:27:37 +0200

    Index sida gjord

---
+++ traeningsDagbok_django/dagbok/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+
+# Create your views here.
+def index(request):
+    return render(request, 'dagbok/index.html')
+
+def register(request):
+    return HttpResponse("REGISTRERA SIG")
+    
+def login(request):
+    return HttpResponse("LOGGA IN")
