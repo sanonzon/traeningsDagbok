d9733836e8b028d0d1c3bb862b241811d95f85ba
Emma Andersson <emmaandersson@Emmas-MacBook-Pro.local>
2016-05-11 16:21:15 +0200

    Dashboard url

--- traeningsDagbok_django/dagbok/urls.py
+++ traeningsDagbok_django/dagbok/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    url(r'^dashboard/$', views.dashboard, name='dashboard'),
     url(r'^register/$', views.register, name='register'),
     url(r'^login/$', views.login, name='login'),
 ]
