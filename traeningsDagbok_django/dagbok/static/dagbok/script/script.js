28d09636a651e6a5285eae181b7f4a76bb0d2a82
Emma Andersson <emmaandersson@Emmas-MacBook-Pro.local>
2016-05-16 18:56:23 +0200

    Snabbtillägg fix på dropdownen och ett litet fint script

--- traeningsDagbok_django/dagbok/static/dagbok/script/script.js
+++ traeningsDagbok_django/dagbok/static/dagbok/script/script.js
@@ -1,3 +1,12 @@
+$(document).ready(function () {
+        $('.fast-workout-sections').hide();
+        // $('#fastWorkoutRunning').show();
+        $('#selectFastWorkout').change(function () {
+            $('.fast-workout-sections').hide();
+            $('#'+$(this).val()).show();
+        });
+    });
+
 document.getElementById('toggleButton').onclick = function() {
     document.getElementsByClassName('menu')[0].classList.toggle('responsive');
 }
